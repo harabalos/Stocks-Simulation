71d0ceea60a48052548ffab5c418215bb097eea5
harabalos <babismetaxas000@gmail.com>
2023-03-04 02:37:12 +0200

    Now gets real time stock

--- main/model/Trade.java
+++ main/model/Trade.java
@@ -2,7 +2,7 @@ package main.model;
 
 public class Trade {
 
-    public enum Stock { aapl, fb, goog, tsla }
+    public enum Stock { AAPL, META, GOOG, TSLA }
     public enum Type { MARKET_BUY, MARKET_SELL }
     
     private Stock stock;
